59f270266a4b696b4a8525afd3cb0d65c4eb6ea0
Nicolas Mejia <Nicolas.Mejia@fundacion-jala.org>
2023-05-31 18:03:17 -0400

    Second-Class-Homework

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -3,21 +3,22 @@ from game.components.enemies.enemy import Enemy
 
 
 class EnemyManager:
-	"""docstring for EnemyManager"""
-	def __init__(self):
-		self.enemies = []
+    def __init__(self):
+        self.enemies = []
 
-	def update(self):
-		self.add_enemy()
+    def update(self):
+        self.add_enemy()
 
-		for enemy in self.enemies:
-			enemy.update(self.enemies)
+        for enemy in self.enemies:
+            enemy.update(self.enemies)
 
-	def draw(self, screen):
-		for enemy in self.enemies:
-			enemy.draw(screen)
-		
-	def add_enemy(self):
-		if len(self.enemies) < 1:
-			enemy = Enemy()
-			self.enemies.append(enemy)
+    def draw(self, screen):
+        for enemy in self.enemies:
+            enemy.draw(screen)
+        
+    def add_enemy(self):
+        if len(self.enemies) < 2:  # Agregar 2 enemigos
+            enemy_1 = Enemy()
+            enemy_2 = Enemy(is_enemy_2=True)
+            self.enemies.append(enemy_1)
+            self.enemies.append(enemy_2)
