a65c138ae35ce7fbfea749db3ba98952dd55614a
Nicolas Mejia <Nicolas.Mejia@fundacion-jala.org>
2023-06-01 00:05:22 -0400

    Third-Class

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,24 +1,31 @@
+import random
+
 from game.components.enemies.enemy import Enemy
 
 
-
 class EnemyManager:
-    def __init__(self):
-        self.enemies = []
+  def __init__(self):
+    self.enemies = []
+    
+  def update(self, game):
+    self.add_enemy()
+    
+    for enemy in self.enemies:
+        enemy.update(self.enemies, game)
+  
+  def draw(self, screen):
+    for enemy in self.enemies:
+        enemy.draw(screen)
+      
+  def add_enemy(self):
+    enemy_type = random.randint(1,2)
+    if enemy_type == 1:
+        enemy = Enemy()
+    else:
+      x_speed = 5
+      y_speed = 2
+      move_x_for = [50, 120]
+      enemy = Enemy(enemy_type, x_speed, y_speed, move_x_for)
 
-    def update(self):
-        self.add_enemy()
-
-        for enemy in self.enemies:
-            enemy.update(self.enemies)
-
-    def draw(self, screen):
-        for enemy in self.enemies:
-            enemy.draw(screen)
-        
-    def add_enemy(self):
-        if len(self.enemies) < 2:  # Agregar 2 enemigos
-            enemy_1 = Enemy()
-            enemy_2 = Enemy(is_enemy_2=True)
-            self.enemies.append(enemy_1)
-            self.enemies.append(enemy_2)
+    if len(self.enemies) < 1:
+        self.enemies.append(enemy)
